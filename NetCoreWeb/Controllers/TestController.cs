794eecbffd7faf11d14a49f82a8ee9bd8a68db66
PHZou <threshold2015@outlook.com>
2017-05-12 17:08:56 +0800

    test

---
+++ NetCoreWeb/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreWeb.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+    }
+}
