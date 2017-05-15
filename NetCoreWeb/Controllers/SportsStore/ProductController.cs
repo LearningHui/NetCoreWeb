3fd4980b4deb1e7dd318711ad00009d053004c17
PHZou <threshold2015@outlook.com>
2017-05-15 18:24:27 +0800

    sportsStore

--- NetCoreWeb/Controllers/SportsStore/ProductController.cs
+++ NetCoreWeb/Controllers/SportsStore/ProductController.cs
@@ -14,6 +14,11 @@ namespace NetCoreWeb.Controllers.SportsStore
         {
             repository = repo;
         }
-        public ViewResult List() => View(repository.Products);
+        public int PageSize = 4;
+        public ViewResult List(int page = 1) => 
+            View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
     }
 }
