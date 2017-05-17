d371f11db01a5445a60df10c0a24b2357ba41632
PHZou <threshold2015@outlook.com>
2017-05-17 17:02:41 +0800

    SportsStore

--- NetCoreWeb/Models/SportsStore/ViewModels/ProductsListViewModel.cs
+++ NetCoreWeb/Models/SportsStore/ViewModels/ProductsListViewModel.cs
@@ -8,5 +8,6 @@ namespace NetCoreWeb.Models.SportsStore.ViewModels
     public class ProductsListViewModel {
         public IEnumerable<Product> Products { get; set; }
         public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
     }
 }
