a89b2905e0624997b745e8663f2a720137b28435
PHZou <threshold2015@outlook.com>
2017-06-12 17:06:08 +0800

    sportsStrore

---
+++ NetCoreWeb/Controllers/SportsStore/OrderController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreWeb.Models.SportsStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreWeb.Controllers.SportsStore
+{
+    public class OrderController : Controller 
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
